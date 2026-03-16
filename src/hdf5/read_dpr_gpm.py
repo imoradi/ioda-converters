12197e16994ae1b7cb2d11109023b7b222b9b050
imoradi <imoradi@umd.edu>
2026-03-15 19:32:10 -0500

    dpr read and average

--- src/hdf5/read_dpr_gpm.py
+++ src/hdf5/read_dpr_gpm.py
@@ -160,7 +160,7 @@ def average_dpr_over_fov_scanline(ds_in,
     dbz_mean = 10 * np.log10(refl_mean)
     dbz_std = 4.343 * (refl_std / refl_mean)
     dbz_std = dbz_std.where(dbz_mean > dbz_threshold, 0)
-
+    cm6m3_std = refl_std * 1e-6
     # --------------------------------------------------
     # Coarsen 2D variables (lat/lon)
     # --------------------------------------------------
@@ -209,7 +209,7 @@ def average_dpr_over_fov_scanline(ds_in,
     ds_out = xr.Dataset()
 
     ds_out["ReflectivityAttenuated"] = dbz_mean
-    ds_out["ReflectivityAttenuated_STD"] = dbz_std
+    ds_out["ReflectivityAttenuated_STD"] = cm6m3_std #dbz_std
 
     ds_out["lat"] = lat_mean
     ds_out["lon"] = lon_mean
